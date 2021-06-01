442a372bba89f006724d70508d877e7c757e51dc
yesthecarlos <carlosmendez86@gmail.com>
2021-06-01 11:12:07 -0700

    Add Authentication
    Co-authored-by: Cristina Plesa <cristinaplesa8@gmail.com>
    Co-authored-by: Nick Reeder <nickreeder32@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -12,6 +12,8 @@ namespace ToDoList.Models
     public int ItemId { get; set; }
     public string Description { get; set; }
 
+    public virtual ApplicationUser User { get; set; }
+
     public virtual ICollection<CategoryItem> JoinEntities { get;}
   }
 }
